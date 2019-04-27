86b3ee8d0e72d59677b2a2d9f8e8c5ef6e797496
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-27 15:08:12 -0700

    Update header.blade.php

--- Backend/resources/views/includes/header.blade.php
+++ Backend/resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light border shadow">
 		<a class="navbar-brand" href="/home">
-			<img src="/public/img/Databae_Logo-01.png">
+			<img src="/img/Databae_Logo-01.png">
 	</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
