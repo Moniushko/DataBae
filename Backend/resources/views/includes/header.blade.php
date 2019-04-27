763610591983bdd7279e855d22da3fcb38081f32
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-27 15:05:27 -0700

    Updated Logo

--- Backend/resources/views/includes/header.blade.php
+++ Backend/resources/views/includes/header.blade.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light border shadow">
-		<a class="navbar-brand" href="/home">DataBae Recipes</a>
+		<a class="navbar-brand" href="/home">
+			<img src="public/img/Databae_logo.png">
+	</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
